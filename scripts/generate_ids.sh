9bf086e405cfbaece325c03faba076aed4bce447
Saalim Quadri <danascape@gmail.com>
2023-07-12 03:16:38 +0530

    CI: Add basic script to generate commit ids
    Signed-off-by: Saalim Quadri <danascape@gmail.com>
    Change-Id: I7b3ccf8a933b2b74de793859e7ce4a14918a9a6a

---
+++ scripts/generate_ids.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+#
+# Copyright (C) 2019 Saalim Quadri (danascape)
+#
+# SPDX-License-Identifier: Apache-2.0 license
+#
+
+# Set Variables
+# Scripts Path
+CI_PATH="$HOME/workspace/stormCI"
+
+# Organization URL
+ORG_URL="https://github.com/stormbreaker-project"
+
+# Repositories
+REPOS="
+	linux-asus-X00P-3.18
+	linux-asus-X01AD
+	"
+
+generate_commit_id()
+{
+	for repo in $REPOS; do
+		COMMIT_ID=$(git ls-remote $ORG_URL/$repo | head -1 | cut -f -1)
+		echo "$COMMIT_ID" >> $CI_PATH/commit-id/$repo-id
+	done
+}
+
+generate_commit_id
